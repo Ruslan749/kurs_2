83cb45af417e95e9270ea23cbbba3deb4cf65c4b
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-04-05 23:33:04 +0500

    individual_work v1.6 theSet

--- example_11/individual_work/list_and_set/Main.java
+++ example_11/individual_work/list_and_set/Main.java
@@ -1,15 +1,8 @@
 package individual_work.list_and_set;
-import individual_work.list_and_set.*;
-import java.util.*;
 public class Main {
     public static void main(String[] args) {
-
-//        SortedSet
-//        SortedSet <Integer> sortedSet = new
-//        TreeSet
-//        Set<Integer> TreeSet = new TreeSet<>();
-
-        arrayList.print ();
+//        arrayList.print ();
+        treeSet.print();
 
     }
 }
--- example_11/individual_work/list_and_set/treeSet.java
+++ example_11/individual_work/list_and_set/treeSet.java
@@ -1,74 +1,32 @@
 package individual_work.list_and_set;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
-public class arrayList {
-    public static void print () {
-        List<Long> arrayList = new ArrayList<>();
+
+public class treeSet {
+    public static void print() {
+        SortedSet<Long> theSet = new TreeSet<>();
         int i = 20;
-        addElements(arrayList, i);
-        System.out.println("Время выполнения операции добавления arrayList  в начале списка = " + addElementsFerst(arrayList, i));
-        System.out.println("Время выполнения операции добавления arrayList  в середине списка = " + insertList(arrayList, i));
-        System.out.println("Время выполнения операции добавления arrayList  в конце списка = " + addElementsLast(arrayList));
 
-        System.out.println("Время выполнения операции удаления arrayList  в начале списка = " + removeElementsFerst(arrayList));
-        System.out.println("Время выполнения операции удаления arrayList  в середине списка = " + removeInsertList(arrayList, i));
-        System.out.println("Время выполнения операции удаления arrayList  в конце списка = " + removeElementsLast(arrayList));
-
-        System.out.println("Время выполнения операции получения элимента arrayList по индексу = " + SearchElements(arrayList, i));
+        System.out.println("Время выполнения операции добавления theSet  в начале списка = " + addElementsFerst(theSet, i));
+        System.out.println("Время выполнения операции добавления theSet  в конце списка = " + addElementsLast(theSet));
+        System.out.println("Время выполнения операции добавления theSet  в середине списка = " + insertSet(theSet));
+//
+        System.out.println("Время выполнения операции удаления theSet  в начале списка = " + removeElementsFerst(theSet));
+        System.out.println("Время выполнения операции удаления theSet  в середине списка = " + removeInsertSet(theSet, i));
+        System.out.println("Время выполнения операции удаления theSet  в конце списка = " + removeElementsSet(theSet));
+//
+        System.out.println("Время выполнения операции получения элимента theSet по индексу = " + SearchElements(theSet));
     }
 
-    private static void addElements(List<Long> list, int i) {
-
-        for (long k = 0; k < i * 1_000_000_0L; k++) {
-            list.add(k);
-        }
-
-    }
-    private static long addElementsFerst(List<Long> list, int i) {
-
+    private static long addElementsFerst(SortedSet<Long> set, long i) {
 
         // точка начала отсчета времени выполнения программы
         long start = System.currentTimeMillis();
 
         // блок кода в котором выполняется операция добавления
-            list.add(0, 30L);
-
-
-        // точка окончания отсчета времени выполнения программы
-        long end = System.currentTimeMillis();
-
-        // вывод в консоль времени выполнения блока кода содержащего операцию
-        return end - start;
-
-    }
-
-    private static long insertList (List<Long> list, int i) {
-
-        // точка начала отсчета времени выполнения программы
-        long start = System.currentTimeMillis();
-
-        // блок кода в котором выполняется операция добавления в серидину колекции
-        list.add(i, 234L);
-
-        // точка окончания отсчета времени выполнения программы
-        long end = System.currentTimeMillis();
-
-        // вывод в консоль времени выполнения блока кода содержащего операцию
-        return end - start;
-
-    }
-
-    private static long addElementsLast (List<Long> list) {
-
-        // точка начала отсчета времени выполнения программы
-        long start = System.currentTimeMillis();
-
-        // блок кода в котором выполняется операция добавления в серидину колекции
-
-        for (long i = 0; i < 1_000_0; i++ ){
-            list.add(list.size()-1, i);
+        for (long k = 0; k < i * 1000_000L; k++) {
+            set.add(k);
         }
 
         // точка окончания отсчета времени выполнения программы
@@ -76,75 +34,107 @@ public class arrayList {
 
         // вывод в консоль времени выполнения блока кода содержащего операцию
         return end - start;
-
     }
 
-    private static long removeElementsFerst(List<Long> list) {
+    private static long addElementsLast(SortedSet<Long> set) {
 
         // точка начала отсчета времени выполнения программы
         long start = System.currentTimeMillis();
 
         // блок кода в котором выполняется операция добавления
-        list.remove(0);
-
-
-        // точка окончания отсчета времени выполнения программы
-        long end = System.currentTimeMillis();
-
-        // вывод в консоль времени выполнения блока кода содержащего операцию
-        return end - start;
-
-    }
-
-    private static long removeInsertList (List<Long> list, int i) {
-
-        // точка начала отсчета времени выполнения программы
-        long start = System.currentTimeMillis();
-
-        // блок кода в котором выполняется операция добавления в серидину колекции
-        list.remove(i);
-
-        // точка окончания отсчета времени выполнения программы
-        long end = System.currentTimeMillis();
-
-        // вывод в консоль времени выполнения блока кода содержащего операцию
-        return end - start;
-
-    }
-
-    private static long removeElementsLast (List<Long> list) {
-
-        // точка начала отсчета времени выполнения программы
-        long start = System.currentTimeMillis();
-
-        // блок кода в котором выполняется операция добавления в серидину колекции
-        for (long i = 0; i < 1_000_0; i++ ){
-            list.remove(list.size()-1);
+        for (long k = 2_2000_000L; k < 2_9000_000L; k++) {
+            set.add(k);
         }
 
-
         // точка окончания отсчета времени выполнения программы
         long end = System.currentTimeMillis();
 
         // вывод в консоль времени выполнения блока кода содержащего операцию
         return end - start;
-
     }
 
-    private static long SearchElements (List<Long> list, int i) {
+    private static long insertSet(SortedSet<Long> set) {
 
         // точка начала отсчета времени выполнения программы
         long start = System.currentTimeMillis();
 
-        // блок кода в котором выполняется операция добавления в серидину колекции
-       System.out.println( list.get(i));
-       System.out.println(list.size());
+        // блок кода в котором выполняется операция добавления
+        for (long k = 2_0000_000L; k < 2_2000_000L; k++) {
+            set.add(k);
+        }
 
         // точка окончания отсчета времени выполнения программы
         long end = System.currentTimeMillis();
 
         // вывод в консоль времени выполнения блока кода содержащего операцию
         return end - start;
+    }
 
+    private static long removeElementsFerst(SortedSet<Long> set) {
+
+        // точка начала отсчета времени выполнения программы
+        long start = System.currentTimeMillis();
+
+        // блок кода в котором выполняется операция добавления
+        for (long k = 0L; k < 400_000L; k++) {
+            set.remove(set.first());
+        }
+
+        // точка окончания отсчета времени выполнения программы
+        long end = System.currentTimeMillis();
+
+        // вывод в консоль времени выполнения блока кода содержащего операцию
+        return end - start;
+    }
+
+    private static long removeInsertSet(SortedSet<Long> set, int i) {
+
+        // точка начала отсчета времени выполнения программы
+        long start = System.currentTimeMillis();
+
+        // блок кода в котором выполняется операция добавления
+        for (long k = 0L; k < 400_000L; k++) {
+            set.tailSet((long) i);
+        }
+
+        // точка окончания отсчета времени выполнения программы
+        long end = System.currentTimeMillis();
+
+        // вывод в консоль времени выполнения блока кода содержащего операцию
+        return end - start;
+    }
+
+    private static long removeElementsSet (SortedSet<Long> set) {
+
+        // точка начала отсчета времени выполнения программы
+        long start = System.currentTimeMillis();
+
+        // блок кода в котором выполняется операция добавления
+        for (long k = 0L; k < 400_000L; k++) {
+            set.remove(set.last());
+        }
+
+        // точка окончания отсчета времени выполнения программы
+        long end = System.currentTimeMillis();
+
+        // вывод в консоль времени выполнения блока кода содержащего операцию
+        return end - start;
+    }
+
+    private static long SearchElements (SortedSet<Long> set) {
+
+        // точка начала отсчета времени выполнения программы
+        long start = System.currentTimeMillis();
+
+        // блок кода в котором выполняется операция добавления
+        for (long k = 0L; k < 400_000L; k++) {
+            set.subSet(5000L,6000L);
+        }
+
+        // точка окончания отсчета времени выполнения программы
+        long end = System.currentTimeMillis();
+
+        // вывод в консоль времени выполнения блока кода содержащего операцию
+        return end - start;
     }
 }
