df9d60a78d45d393d3e61bd90407d1bded64038e
ruslan749 <73393054+Ruslan749@users.noreply.github.com>
2023-04-05 23:33:44 +0500

    individual_work v1.7 theSet

--- example_11/individual_work/list_and_set/Main.java
+++ example_11/individual_work/list_and_set/Main.java
@@ -1,7 +1,7 @@
 package individual_work.list_and_set;
 public class Main {
     public static void main(String[] args) {
-//        arrayList.print ();
+        arrayList.print ();
         treeSet.print();
 
     }
